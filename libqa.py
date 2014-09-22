8a11aa86fa94811acf8968b88804d21283336a89
szendeh <szendeh@mit.edu>
2014-09-22 14:20:16 -0400

    code refactor

--- libqa.py
+++ libqa.py
@@ -13,49 +13,94 @@ logging.basicConfig(level       = logging.INFO,
 logger = logging
 
 def testLine(line):
-    print line
+    try:
+        r = requests.get(line)
+        if (r.status_code == 200):
+            return True
+        else:
+            print "{} '{}'".format('status:', r.status_code)
+            return False
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
+        print e
+        return False
 
 def processFiles(pid):
+    """once there are files in a temp dir, this will:
+    1. build a list of all urls to test ('process list')
+    2. opens any existing success file
+        - if none exists, create a new one
+        - build an array of already succeeded urls from that file
+    3. opens a new failed file for writing, overwriting the old file
+    4. opens the success file for appending
+    5. iterates through process list
+        - skip if url exists in success list
+        - test the url
+        - if success, add url to success file
+        - if failed, add to failed file
+    6. move the failed file back to app root
+    7. clean up temp dir
+    """
+
     process_dirname     = 'process_'+ pid
     success_filename    = process_dirname +'/'+ 'process_success_list.txt'
-    error_filename      = process_dirname +'/'+ 'process_error_list.txt'
+    failed_filename     = process_dirname +'/'+ 'process_failed_list.txt'
 
-    file_list = fnmatch.filter(os.listdir(process_dirname), 'new*.txt') + fnmatch.filter(os.listdir(process_dirname), 'error*.txt')
+    file_list = fnmatch.filter(os.listdir(process_dirname), 'new*.txt') + fnmatch.filter(os.listdir(process_dirname), 'failed*.txt')
 
     if (len(file_list)):
         try:
-            # make sure we capture the list of already-processed files (if any)
-            success_list = []
+            # maintain a list of already-tested links *for this run*
+            # note that this can be different than just the success_file list
+            # because we may simply have duplicate urls, and we don't want to bother
+            # re-testing them in any single run, whether or not they succeed
+            tested_list = []
 
             if (os.path.isfile(success_filename)):
                 with open(success_filename, 'r') as success_file:
                     for line in success_file:
-                        success_list.push(line)
+                        tested_list.append(line.strip())
 
-            with open(success_filename, 'a') as success_file, open(error_filename, 'w') as error_file:
+            with open(success_filename, 'a') as success_file, open(failed_filename, 'w') as failed_file:
                 for filename in file_list:
                     logger.info('processing file: '+ filename)
 
                     with open(process_dirname +'/'+ filename, 'r') as processing_file:
                         for line in processing_file:
-                            if (line not in success_list):
-                                if (testLine(line) === True):
-                                    success_file.write(line)
-                                else:
-                                    error_file.write(line)
-        except IOError as e:
-            print 'ERROR: '+ e
+                            line = line.strip()
 
-        try:
-            if (os.path.getsize(error_filename)):
-                shutil.move(error_filename, './error_'+ )
+                            if (line == ''):
+                                logger.info('blank line')
+                            elif (line in tested_list):
+                                logger.info("duplicate line '"+ line +"'")
+                            else:
+                                logger.info("testing url '"+ line +"'")
+
+                                if (testLine(line) == True):
+                                    success_file.write(line +"\n")
+                                else:
+                                    failed_file.write(line +"\n")
+
+                                tested_list.append(line)
+
+            if (os.path.getsize(failed_filename)):
+                shutil.move(failed_filename, './failed_'+ pid +'.txt')
+
+            shutil.rmtree(process_dirname)
+        except IOError as e:
+            logger.error(e)
 
 def moveFiles(pid):
+    """when there are existing files in the app root dir
+    the app will move them to a temp dir for processing
+    this eliminates the chance that running this app twice
+    will cause problems"""
+
     process_dirname = 'process_'+ pid
 
-    file_list = fnmatch.filter(os.listdir('.'), 'new*.txt') + fnmatch.filter(os.listdir('.'), 'error*.txt')
+    file_list = fnmatch.filter(os.listdir('.'), 'new*.txt') + fnmatch.filter(os.listdir('.'), 'failed*.txt')
 
-    logger.info(__name__ +' found files: '+ ','.join(file_list))
+    logger.info('found files: '+ ','.join(file_list))
 
     if (len(file_list)):
         try:
@@ -65,22 +110,27 @@ def moveFiles(pid):
             for filename in file_list:
                 shutil.move(filename, process_dirname +'/'+ filename)
         except IOError as e:
-            print 'ERROR: '+ e
+            logger.error(e)
 
     return(file_list)
 
-logger.info('start '+ sys.argv[0])
+def main():
+    logger.info('start '+ sys.argv[0])
 
-if (sys.argv[1]):
     # specified an existing run to re-try
-    pid = sys.argv[1]
+    if (len(sys.argv)>1):
+        pid = sys.argv[1]
 
-    processFiles(pid)
-else:
-    # kick off a fresh run
-    pid = time.strftime("%Y%m%d_%H%M%S")
-
-    if (len(moveFiles(pid))):
         processFiles(pid)
 
-logger.info('end '+ sys.argv[0])
+    # kick off a fresh run
+    else:
+        pid = time.strftime("%Y%m%d_%H%M%S")
+
+        if (len(moveFiles(pid))):
+            processFiles(pid)
+
+    logger.info('end '+ sys.argv[0])
+
+if __name__ == "__main__":
+   main()
